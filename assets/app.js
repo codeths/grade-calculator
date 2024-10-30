20ea5e0ea9c13ff56c0a13d4ced10de648ccb6e5
Eli Coustan <49954272+edcous@users.noreply.github.com>
2024-10-30 17:19:45 +0000

    add correct letter grades and percentage calculations
    Co-authored-by: Sparrow Biss <Sparr0w0@users.noreply.github.com>
    Co-authored-by: Pab200 <Pab200@users.noreply.github.com>
    Co-authored-by: eveabraham <eveabraham@users.noreply.github.com>

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,65 @@
 let q1 = document.getElementById("Quarter1Grade")
+if(q1.value > 100){
+    q1.value = 100
+}
 let q2 = document.getElementById("Quarter2Grade")
+if(q2.value > 100){
+    q2.value = 100
+}
 let output = document.getElementById("output")
 let input_box = document.getElementById("custom_input")
 
-let table_rows = [50, 60, 70, 80, 90, 100]
+let table_rows = [65, 70, 75, 80, 85, 90, 95, 100]
 
 function calculateGrade(final_grade){
-    return q1.value * .45 + q2.value * .45 + final_grade * .1;
+    let grade = q1.value * .45 + q2.value * .45 + final_grade * .1;
+    return Math.round(grade*100)/100
+}
+
+function calculateLetterGrade(semester_grade){
+    if(semester_grade >= 92.5){
+        return "A"
+    }
+    else if(semester_grade >= 89.5){
+        return "A-"
+    }
+    else if(semester_grade >= 86.5){
+        return "B+"
+    }
+    else if(semester_grade >= 82.5){
+        return "B"
+    }
+    else if(semester_grade >= 79.5){
+        return "B-"
+    }
+    else if(semester_grade >= 76.5){
+        return "C+"
+    }
+    else if(semester_grade >= 72.5){
+        return "C"
+    }
+    else if(semester_grade >= 69.5){
+        return "C-"
+    }
+    else if(semester_grade >= 66.5){
+        return "D+"
+    }
+    else if(semester_grade >= 59.5){
+        return "D"
+    }
+    return "F"
+}
+
+function generateString(final_grade){
+    const semester_grade = calculateGrade(final_grade)
+    return `${calculateLetterGrade(semester_grade)} (${semester_grade}%)`
 }
 
 function onChange(){
     table_rows.forEach(final_grade => {
         let table_row = document.getElementById(final_grade.toString())
-        table_row.innerHTML = calculateGrade(final_grade) 
+        table_row.innerHTML = generateString(final_grade) 
     });
     let table_row = document.getElementById("custom_result")
-    table_row.innerHTML = calculateGrade(input_box.value)
+    table_row.innerHTML = generateString(input_box.value)
 }
