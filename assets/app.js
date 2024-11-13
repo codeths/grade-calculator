cffaae4a2e227c1cd99a7f460d104cd1ccbc3098
Eli Coustan <49954272+edcous@users.noreply.github.com>
2024-11-13 21:32:13 +0000

    2 decimal place rounding

--- assets/app.js
+++ assets/app.js
@@ -51,14 +51,17 @@ function generateString(final_grade){
 }
 
 function onChange(){
-    let quarter1 = parseInt(q1.value)
-    let quarter2 = parseInt(q2.value)
-    let final = parseInt(input_box.value)
+    let quarter1 = parseFloat(q1.value)
+    let quarter2 = parseFloat(q2.value)
+    let final = parseFloat(input_box.value)
     if(quarter1 < 50){
         q1.value = 50
     }
     else if(quarter1 > 100){
         q1.value = 100
+    }
+    else{
+        q1.value = Math.round((quarter1 + Number.EPSILON) * 100) / 100
     }
     if(quarter2 < 50){
         q2.value = 50
@@ -66,12 +69,19 @@ function onChange(){
     else if(quarter2 > 100){
         q2.value = 100
     }
+    else{
+        q2.value = Math.round((quarter2 + Number.EPSILON) * 100) / 100
+    }
     if(final < 0){
         input_box.value = 0
     }
     else if(final > 100){
         input_box.value = 100
     }
+    else{
+        input_box.value = Math.round((final + Number.EPSILON) * 100) / 100
+    }
+
     numberValidation({key: ""})
     table_rows.forEach(final_grade => {
         let table_row = document.getElementById(final_grade.toString())
